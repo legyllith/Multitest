afbb03306b1e078dd7f58954214bf37ba5b3bb89
legyllith <jeremy.n.49@live.fr>
2020-05-20 11:17:10 +0200

    test node

---
+++ multitest/Assets/Scripts/Player/CanvasPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasPlayer : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.color = new Color(Random.Range(0F, 1F), Random.Range(0, 1F), Random.Range(0, 1F));
+    }
+
+   
+}
